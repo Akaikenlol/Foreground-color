0aeb8f2d6de30772c56a29fc84979792e3f56d03
Akaikenlol <67327997+Akaikenlol@users.noreply.github.com>
2020-07-05 14:37:49 +0630

    Add files via upload

---
+++ Foreground colours/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Foreground_colours
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(Console.BackgroundColor==ConsoleColor.Black){
+                Console.BackgroundColor=ConsoleColor.Red;
+                Console.ForegroundColor=ConsoleColor.Black;
+                Console.Clear();
+            }
+        }
+    }
+}
